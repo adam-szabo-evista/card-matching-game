7ae96f83c9dee84af697ad727c69388c20d2c6ac
Szabó Ádám <szabo.adam@e-vista.hu>
2020-11-05 17:01:47 +0100

    Added button type variants.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 import './Button.scss';
 
-const Button = ({ children, onClick }) => {
-  return <div className='app-button' onClick={onClick}>
+const Button = ({ children, onClick, type = 'primary' }) => {
+  const classes = ` ${type}${typeof onClick === 'function' ? ' clickable' : ''}`;
+
+  return <div className={`app-button${classes}`} onClick={onClick}>
     <span>{children}</span>
   </div>
 };
